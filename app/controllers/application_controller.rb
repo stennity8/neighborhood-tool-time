ab50dee1bf1b658be57beff2c5a3560c027afd27
Shawn <shawntennity@gmail.com>
2019-09-10 12:44:45 -0400

    Added additional params to devise sanitizer to allow for the additional user attributes.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+      def configure_permitted_parameters
+          devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :street_address, :city, :state, :zip_code, :email, :password]) 
+          devise_parameter_sanitizer.permit(:account_update, keys: [:name, :street_address, :city, :state, :zip_code, :email, :password, :current_password]) 
+      end
 end
