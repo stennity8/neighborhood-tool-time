d2f3bd42f8777d9dd584c80dc7ae922affc13701
Shawn <shawntennity@gmail.com>
2019-09-12 19:59:14 -0400

    Created user controller and nested routes for user/equipment.  Started creating equipment index view.

--- app/controllers/equipment_controller.rb
+++ app/controllers/equipment_controller.rb
@@ -1,2 +1,7 @@
 class EquipmentController < ApplicationController
+   before_action :authenticate_user!
+
+  def index
+    @user = current_user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   root :to => "welcome#index"
   
-  resources :equipment_categories
-  resources :categories
-  resources :borrows
-  resources :equipment
-  #resources :users, only: [:show]
-  
+  #resources :equipment_categories
+  #resources :categories
+  #resources :borrows
+  #resources :equipment
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:show] do
+    resources :equipment, only: [:index]
+  end
 
 end
