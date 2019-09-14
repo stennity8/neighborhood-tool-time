3d00a7613c5e03e6c047590d7bc99ea9558382ea
Shawn <shawntennity@gmail.com>
2019-09-14 17:23:28 -0400

    Set up routes and partials for user's borrowed tool's page.

--- app/controllers/borrows_controller.rb
+++ app/controllers/borrows_controller.rb
@@ -1,6 +1,10 @@
 class BorrowsController < ApplicationController
+  before_action :authenticate_user!
 
   def borrowed_equipment
+    @equipment = current_user.equipments
+    @borrows = current_user.borrows
+    
   end
 
   def lent_equipment
